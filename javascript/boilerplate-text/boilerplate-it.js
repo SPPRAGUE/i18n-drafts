b9734bac2825d6459928481c18db1e9ca113b984
r12a <ishida@w3.org>
2025-05-10 08:18:58 +0100

    boilerplate-it   Apply new order. List needed translations. Add Persian.

--- javascript/boilerplate-text/boilerplate-it.js
+++ javascript/boilerplate-text/boilerplate-it.js
@@ -12,12 +12,13 @@ s.rtlAttribute = ""
 s.ltrAttribute = ""
 s.rlm = ""
 
-
 // TRANSLATE THE FOLLOWING
+// but do not translate anything inside ${...} – but do move those items where needed to fit the syntax of the translation
 
 
 // used when changing the language of the page
 // do not translate the abbreviation at the start of the line, just translate the language name
+
 s.currLang = {
   'ar':'Arabo',
   'bg':'Bulgaro',
@@ -35,6 +36,7 @@ s.currLang = {
   'ja':'Giapponese',
   'ko':'Coreano',
   'nl':'Olandese',
+  'pes':'Persiano',
   'pl':'Polacco',
   'pt':'Portoghese',
   'pt-br':'Portoghese',
@@ -46,20 +48,23 @@ s.currLang = {
   'uk':'Ucraino',
   'vi':'Vietnamita',
   'zh-hans':'Cinese semplificato',
-  'zh-hant':'Cinese tradizionale',
+  'zh-hant':'Cinese tradizionale'
 }
+
 // cookie message, appears when changing languages
 s.cookieMsg = "Se consenti al browser di impostare un cookie, continuerai a visualizzare le pagine W3C Internationalization Activity (ove disponibili) nella lingua selezionata. Vuoi impostare il cookie?" // this text is to be copied to another location
-
 
 // items in top right beige column
 s.worldMap = "Mappa del mondo"  // title text for image, top right
 s.searchI18nSite = "Ricerca nel sito I18n" // placeholder text for search box, top right
-s.translationDisclaimer = 'Questo documento è una traduzione. In caso di differenze o errori, dovrebbe essere considerato la fonte più autorevole <a href="'+f.filename+'.en">l\'ultimo documento originale Inglese</a>. <a href="#copyright">Il copyright originale</a> appartiene al W3C, come mostrato in basso.'  // text appears only on translated pages
+s.translationDisclaimer = `Questo documento è una traduzione. In caso di differenze o errori, dovrebbe essere considerato la fonte più autorevole <a href="${ f.filename }.en">l'ultimo documento originale Inglese</a>. <a href="#copyright">Il copyright originale</a> appartiene al W3C, come mostrato in basso.`  // text appears only on translated pages
+
 s.articles = "Articoli" // used in breadcrumbs, top right of page
 s.tests="Tests" // used at the end of breadcrumbs for test related pages
 s.home = "Home" // start of breadcrumbs
 s.onThisPage = "su questa pagina"  // TOC heading
+
+
 
 
 // messages that appear in the top right beige column before an article is published
@@ -109,15 +114,15 @@ s.previousAuthors = "Prima versione di: " // followed by name of previous author
 s.modifiedBy = "Modificato da: " // person's name appears after colon
 s.translatedBy = "Traduttore: " // person's name appears after colon
 s.acknowledgements = "Un ringraziamento anche a coloro il cui contributo o riscontro é stato incluso: " // used at bottom of page to list people who provided feedback. The list comes after this text. 
-s.translatedFromEnglishVer = "Tradotto dal contenuto Inglese datato "+dt.enVersion+". Ultima modifica della versione tradotta "+dt.thisVersionPlain+" GMT"
+s.translatedFromEnglishVer = `Tradotto dal contenuto Inglese datato ${ dt.enVersion }. Ultima modifica della versione tradotta ${ dt.thisVersionPlain } GMT`
 s.translation_updated="Traduzione aggiornata: " // date appears after (add colon+whitespace) 
-s.historyOfDocumentChanges = 'Per la cronologia delle modifiche apportate al documento, vedi  <a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">news feed</a> fper le modifiche sostanziali e <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/'+f.directory+f.filename+'.en.html">la lista dei commit Github</a> per tutte le modifiche apportate da gennaio 2016.'
+s.historyOfDocumentChanges = `Per la cronologia delle modifiche apportate al documento, vedi  <a href="http://www.w3.org/blog/International/tag/${ f.searchString }/">news feed</a> fper le modifiche sostanziali e <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/${ f.directory }${ f.filename }.en.html">la lista dei commit Github</a> per tutte le modifiche apportate da gennaio 2016.`
 
 
 // banner on translated pages that are missing some translation
-s.untranslatedChanges = '<strong>Nota:</strong> Sono state apportate delle modifiche alla <a href="'+f.filename+'.en">versione originale in inglese</a> dal momento in cui il presente documento é stato tradotto. Le modifiche sono state inserite nella presente pagina. Per tale ragione, potrai visualizzare alcuni passaggi in inglese fino a che non aggiornerai la traduzione.'
-s.unlinkedTranslation = '<strong>Avviso:</strong> Delle modifiche sostanziali sono state apportate alla versione originale in inglese dal momento in cui il presente documento é stato tradotto. Ti consigliamo di leggere <a href="'+f.filename+'.en">la versione originale in inglese</a> fino al momento in cui la traduzione non é stata aggiornata.'
-s.githubRedirect = "<strong>NOTA!</strong> &nbsp; Questo é solo una bozza dell’articolo del redattore. Tutti i link e i preferiti dovrebbero rimandare a <a href='https://www.w3.org/International/"+f.directory+f.filename+"."+f.clang+"'>versione sul sito di W3C</a>." // used for github-based versions of published articles
+s.untranslatedChanges = `<strong>Nota:</strong> Sono state apportate delle modifiche alla <a href="${ f.filename }.en">versione originale in inglese</a> dal momento in cui il presente documento é stato tradotto. Le modifiche sono state inserite nella presente pagina. Per tale ragione, potrai visualizzare alcuni passaggi in inglese fino a che non aggiornerai la traduzione.`
+s.unlinkedTranslation = `<strong>Avviso:</strong> Delle modifiche sostanziali sono state apportate alla versione originale in inglese dal momento in cui il presente documento é stato tradotto. Ti consigliamo di leggere <a href="${ f.filename }.en">la versione originale in inglese</a> fino al momento in cui la traduzione non é stata aggiornata.`
+s.githubRedirect = `<strong>NOTA!</strong> &nbsp; Questo é solo una bozza dell’articolo del redattore. Tutti i link e i preferiti dovrebbero rimandare a <a href='https://www.w3.org/International/${ f.directory }${ f.filename }.${ f.clang }'>versione sul sito di W3C</a>.` // used for github-based versions of published articles
 
 
 s.new="Nuovo"// placed alongside new articles at https://www.w3.org/International/articlelist 
@@ -164,7 +169,7 @@ s.codedInUtf8 = "Codificato in UTF-8!"
 // other — may be obsolete, or used in old format pages
 s.i18nActivityHome = "Home dell'I18N Activity"
 s.moreResourcesOfThisType = "Altre risorse di questo tipo."
-s.accessKeyN = 'Accesskey n salta alla <a href="#internal-links" accesskey="n">navigazione della pagina</a>. <a href="#contentstart">Salta al contentuo.</a>'
+s.accessKeyN = `Accesskey n salta alla <a href="#internal-links" accesskey="n">navigazione della pagina</a>. <a href="#contentstart">Salta al contentuo.</a>`
 s.examplesInAnotherScript = "Questo documento contiene esempi in un'altra lingua."
 s.relatedLinks = "Link correlati"
 s.techIndexText = "Indice delle Tecniche"
@@ -172,3 +177,15 @@ s.topicIndexText = "Indice dei Temi"
 s.i18nActivityHomePage = "Vai alla Home Page di Internationalization."
 s.topicIndexForInformation = "Indice dei Temi e delle Risorse."
 s.topics = "Temi"
+
+
+
+/*
+
+MISSING TRANSLATIONS
+
+
+
+
+
+*/
