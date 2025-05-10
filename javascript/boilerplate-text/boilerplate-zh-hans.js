8028a0e9f3ebae8180d6781a5b093eaa7b92f88d
r12a <ishida@w3.org>
2025-05-10 12:34:15 +0100

    boilerplate-zh-hans/hant   Apply new order. List needed translations. Add Persian.

--- javascript/boilerplate-text/boilerplate-zh-hans.js
+++ javascript/boilerplate-text/boilerplate-zh-hans.js
@@ -12,8 +12,13 @@ s.rtlAttribute = ""
 s.ltrAttribute = ""
 s.rlm = ""
 
-
 // TRANSLATE THE FOLLOWING
+// but do not translate anything inside ${...} – but do move those items where needed to fit the syntax of the translation
+
+
+// used when changing the language of the page
+// do not translate the abbreviation at the start of the line, just translate the language name
+
 s.currLang = {
   'ar':'阿拉伯语',
   'bg':'保加利亚语',
@@ -31,6 +36,7 @@ s.currLang = {
   'ja':'日语',
   'ko':'韩语',
   'nl':'荷兰语',
+  'pes':'波斯语',
   'pl':'波兰语',
   'pt':'葡萄牙语',
   'pt-br':'巴西葡萄牙语',
@@ -42,102 +48,143 @@ s.currLang = {
   'uk':'乌克兰语',
   'vi':'越南语',
   'zh-hans':'简体中文',
-  'zh-hant':'繁体中文',
+  'zh-hant':'繁体中文'
 }
 
-s.i18nActivityHome = "I18N 活动主页"
-s.moreResourcesOfThisType = "更多本类资源"
-s.accessKeyN = '快捷键及跳转页面导航。<a href="#contentstart">跳转到正文开始。</a>'
-s.examplesInAnotherScript = "本文档包括其他语言/字符示例。"
-s.worldMap = "世界地图"
-s.searchI18nSite = "国际化站点搜索" 
-s.translationDisclaimer = '本文档译自其他语种。如有任何偏差或错误，请以<a href="'+f.filename+'.en">最新英文原文</a>为准。如下所示，原文版权属于 W3C。'
-s.relatedLinks = "相关链接"
-s.articles = "文章" // used in breadcrumbs, top right of page 
-s.topicIndexText = ""
-s.techIndexText = "技术索引"
-s.gotoW3cHome = "访问 W3C 主页"
-s.gotoI18nHome = "访问国际化标准计划主页"
-s.internationalizationTitle = "国际化"
-s.worldwide = "缔造真正全球通行的万维网"
-s.i18nActivityHomePage = "国际化标准计划主页。"
-s.home = "主页"
-s.contactI18nActivity = "从 W3C 国际化获得帮助。"
-s.contact = "联系我们"
-s.aboutI18nActivity = "About W3C Internationalization."
-s.about = "关于"
-s.askI18nActivity = "Ask for help or information."
-s.ask = "提问"
-s.groupsThatMakeUp = "参与 W3C 国际化工作。"
-s.groups = "参与"
-s.topicIndexForInformation = "站内搜索"
-s.topics = "搜索"
-s.taskBasedIndex = "学习国际化的技术和需求。"
-s.techniques = "学习"
-s.informationResources = "在此网站上查找信息。"
-s.resources = "发现"
-s.newsFiltersAndFeeds = "关注 W3C 国际化的工作。"
-s.news = "关注"
-s.onThisPage = "目录"
-s.questionAlt = "问题"
-s.questionLink = "问题"
-s.question = "问题"
-s.backgroundAlt = "背景信息"
-s.backgroundLink = "背景"
-s.background = "背景"
-s.answer = "答案" // heading
-s.answerAlt = "答案"
-s.answerLink = "答案"
-s.byTheWayAlt = "其他有用信息"
-s.byTheWayLink = "其他信息"
-s.byTheWay = "其他信息"
-s.furtherReadingAlt = "深入阅读"
-s.furtherReadingLink = "深入阅读"
-s.furtherReading = "深入阅读"
-s.intendedAudience = "目标读者:"
-s.skipToAnswer = "[跳转到答案]"
-s.tellUsWhatYouThink = "告诉我们您的想法（用英语）。"
-s.sendAComment = "将您的评论发送给我们"
-s.subscribeToRSS = "订阅 RSS 提要"
-s.newResourcesAlt = "每当首次发布一个新的资源时通知您。"
-s.newResources = "新的资源"
-s.homePageNewsAlt = "主页上显示的所有新闻条目。."
-s.homePageNews = "主页新闻"
-s.followOurNews = "关注我们的新闻源。"
-s.sentenceDelimiter = "."
+// cookie message, appears when changing languages
+s.cookieMsg = "您可以让浏览器设置cookie，以继续使用您选择的语言浏览W3C国际化标准计划（如果有该语言的翻译的话）。请问您想设置cookie吗？" // this text is to be copied to another location
 
-s.validXHTML = "正确的 XHTML 1.0!"
-s.validCSS = "正确的 CSS!"
-s.codedInUtf8 = "UTF-8 编码!"
+// items in top right beige column
+s.worldMap = "世界地图"  // title text for image, top right
+s.searchI18nSite = "国际化站点搜索" // placeholder text for search box, top right
+s.translationDisclaimer = `本文档译自其他语种。如有任何偏差或错误，请以<a href="${ f.filename }.en">最新英文原文</a>为准。如下所示，原文版权属于 W3C。`  // text appears only on translated pages
 
-// this block of strings should include any whitespace needed after the colon
-// or its equivalent, eg. "By: " in english, but "作者：" in chinese
-s.author = "作者：" // followed by name of author(s)
-s.previousAuthors = "前作者：" // followed by name of previous author(s), before substantive changes were made by the current author
-s.modifiedBy = "修改：" // person's name appears after colon
-s.translatedBy = "翻译："
-s.acknowledgements = "还要感谢以下人员的贡献或反馈：" // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.articles = "文章" // used in breadcrumbs, top right of page
+s.tests="测试" // used at the end of breadcrumbs for test related pages
+s.home = "主页" // start of breadcrumbs
+s.onThisPage = "目录"  // TOC heading
 
-s.translatedFromEnglishVer = "翻译以格林尼治标准时间 "+dt.enVersion+" 的英语内容为准，并于格林尼治标准时间 "+dt.thisVersionPlain+" 作最新修改。"
 
-s.historyOfDocumentChanges = '有关文档更新的历史记录，请参阅主要更新的<a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">新闻源</a>，以及自2016年1月以来所有更改的 <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/'+f.directory+f.filename+'.en.html">Git 提交列表</a>。'
-s.untranslatedChanges = '<strong>注意：</strong>在此文件翻译以后<a href="'+f.filename+'.en.php">英文版本</a> 已有改变。这些更改已合并到此页面中，因此您可能会看到一些英文段落，直到翻译更新为止。'
-s.unlinkedTranslation = '<strong>警告：</strong>本文的英文原文自翻译以来进行了实质性修改，建议您在此版本更新之前阅读<a href="'+f.filename+'.en">英文版本</a>。'
-s.new="新"// placed alongside new articles at http://www.w3.org/International/articlelist 
-s.updated="更新：" // same as New
-s.translation_updated="更新译本:" // date appears after colon 
 
+
+// messages that appear in the top right beige column before an article is published
 s.aboutThisArticle="文章相关" // title in the right column near the top of the page
 s.status_draft="此篇文章为未经公开审阅的拟定草稿。如有信息需要及时补充或订正，请您使用页面下的网络链接及时反馈给我们。"
 s.status_review="此篇文章目前正供公开审阅，请发送您的宝贵意见到 <a href='mailto:www-international@w3.org?subject=%5Breview%20feedback%5D%20$filename'>www-international@w3.org</a><span class='noprint'> (<a href='https://lists.w3.org/Archives/Public/www-international/'>订阅</a>)</span>."
 s.status_published="此篇文章已由 W3C 国际化工作组（W3C Internationalization Working Group）审阅，并经过公开审阅以尽可能的确保文章的正确性。如有信息需要及时补充或订正，请您使用页面下的网络链接及时反馈给我们。"
 s.status_notreviewed="此篇文章尚未经过公开审阅。如有信息需要及时补充或订正，请您使用页面下的网络链接及时反馈给我们。"
 s.status_obsolete="这篇文章现已过时，不会继续维护，且信息可能不准确。有关更多最新信息，请参阅<a href='http://www.w3.org/International/'>国际化标准计划主页</a>。"
-s.tests="测试" // used at the end of breadcrumbs for test related pages
+
+
+// top left of page
+s.gotoW3cHome = "访问 W3C 主页"  // title text for W3C logo
+s.gotoI18nHome = "访问国际化标准计划主页" // title text for i18n banner
+s.internationalizationTitle = "国际化"  // the word above the orange line
+s.worldwide = "缔造真正全球通行的万维网"  // the words below the orange line
+s.techniques = "学习"  // site links link text, top left
+s.taskBasedIndex = "学习国际化的技术和需求。"  // title text for s.techniques
+s.resources = "发现"  // site links link text, top left
+s.informationResources = "在此网站上查找信息。"  // title text for s.resources
+s.ask = "提问"  // site links link text, top left
+s.askI18nActivity = "Ask for help or information."  // title text for s.ask
+s.news = "关注"  // site links link text, top left
+s.newsFiltersAndFeeds = "关注 W3C 国际化的工作。"  // title text for s.news
+s.groups = "参与" // site links link text, top left
+s.groupsThatMakeUp = "参与 W3C 国际化工作。" // title text for s.groups
+s.about = "关于" // site links link text, top left
+s.aboutI18nActivity = "About W3C Internationalization." // title text for s.about
+
+
+// document status information, below main heading
+s.intendedAudience = "目标读者:"  // preface to audience description (being faded out)
+s.lastChanged = "最后一次更新 "
+
+
+// bottom right comment and news feed box
+s.tellUsWhatYouThink = "告诉我们您的想法（用英语）。"  // text content
+s.sendAComment = "将您的评论发送给我们" // link text
+
+
+// page footer
+// this block of strings should include any whitespace needed after the colon
+// or its equivalent, eg. "By: " in english, but "作者：" in chinese
+s.sentenceDelimiter = "."  // provides a sentence terminator to use between list of authors, modifiers, & translators
+s.author = "作者：" // followed by name of author(s)
+s.previousAuthors = "前作者：" // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "修改：" // person's name appears after colon
+s.translatedBy = "翻译：" // person's name appears after colon
+s.acknowledgements = "还要感谢以下人员的贡献或反馈：" // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.translatedFromEnglishVer = `翻译以格林尼治标准时间 ${ dt.enVersion } 的英语内容为准，并于格林尼治标准时间 ${ dt.thisVersionPlain } 作最新修改。`
+s.translation_updated="更新译本:" // date appears after (add colon+whitespace) 
+s.historyOfDocumentChanges = `有关文档更新的历史记录，请参阅主要更新的<a href="http://www.w3.org/blog/International/tag/${ f.searchString }/">新闻源</a>，以及自2016年1月以来所有更改的 <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/${ f.directory }${ f.filename }.en.html">Git 提交列表</a>。`
+
+
+// banner on translated pages that are missing some translation
+s.untranslatedChanges = `<strong>注意：</strong>在此文件翻译以后<a href="${ f.filename }.en.php">英文版本</a> 已有改变。这些更改已合并到此页面中，因此您可能会看到一些英文段落，直到翻译更新为止。`
+s.unlinkedTranslation = `<strong>警告：</strong>本文的英文原文自翻译以来进行了实质性修改，建议您在此版本更新之前阅读<a href="${ f.filename }.en">英文版本</a>。`
+s.githubRedirect = `<strong>注意！</strong> &nbsp; 这只是本文的编辑草案，所有链接和收藏都应指向 <a href='https://www.w3.org/International/${ f.directory }${ f.filename }.${ f.clang }'>W3C 网站上的版本</a>。` // used for github-based versions of published articles
+
+
+s.new="新"// placed alongside new articles at https://www.w3.org/International/articlelist 
+s.updated="更新：" // same as New
+
+
+// strings to promote consistency in article content
+s.question = "问题"  // heading
+s.questionAlt = "问题"  // title text for s.question
+s.questionLink = "问题"  // 
+s.skipToAnswer = "[跳转到答案]"  // link text
+s.background = "背景" // heading
+s.backgroundAlt = "背景信息" // title text for s.background
+s.backgroundLink = "背景"
+s.answer = "答案" // heading
+s.answerAlt = "答案" // title text for s.answer
+s.answerLink = "答案"
+s.byTheWay = "其他信息" // heading
+s.byTheWayAlt = "其他有用信息" // title text for s.byTheWay
+s.byTheWayLink = "其他信息"
+s.furtherReading = "深入阅读" // heading
+s.furtherReadingAlt = "深入阅读" // title text for s.byTheWay
+s.furtherReadingLink = "深入阅读"
 s.quickanswer = "简约回复" // heading
 s.longeranswer = "详情" // heading that follows 'Quick answer'
 s.additionalinfo = "更多信息" // heading that sometimes follows 'Details'
-s.cookieMsg = "您可以让浏览器设置cookie，以继续使用您选择的语言浏览W3C国际化标准计划（如果有该语言的翻译的话）。请问您想设置cookie吗？" // this text is to be copied to another location
-s.githubRedirect = "<strong>注意！</strong> &nbsp; 这只是本文的编辑草案，所有链接和收藏都应指向 <a href='https://www.w3.org/International/"+f.directory+f.filename+"."+f.clang+"'>W3C 网站上的版本</a>。" // used for github-based versions of published articles
 
-s.lastChanged = "最后一次更新 "
+
+// obsolete in most recent articles — used to be in bottom right box
+s.subscribeToRSS = "订阅 RSS 提要"  
+s.newResourcesAlt = "每当首次发布一个新的资源时通知您。"
+s.newResources = "新的资源"
+s.homePageNewsAlt = "主页上显示的所有新闻条目。."
+s.homePageNews = "主页新闻"
+s.followOurNews = "关注我们的新闻源。"
+
+// obsolete in most recent articles — used to be in footer
+s.validXHTML = "正确的 XHTML 1.0!"
+s.validCSS = "正确的 CSS!"
+s.codedInUtf8 = "UTF-8 编码!"
+
+
+
+// other — may be obsolete, or used in old format pages
+s.i18nActivityHome = "I18N 活动主页"
+s.moreResourcesOfThisType = "更多本类资源"
+s.accessKeyN = `快捷键及跳转页面导航。<a href="#contentstart">跳转到正文开始。</a>`
+s.examplesInAnotherScript = "本文档包括其他语言/字符示例。"
+s.relatedLinks = "相关链接"
+s.techIndexText = "技术索引"
+s.topicIndexText = ""
+s.i18nActivityHomePage = "国际化标准计划主页。"
+s.topicIndexForInformation = "站内搜索"
+s.topics = "搜索"
+
+
+
+/*
+
+MISSING TRANSLATIONS
+
+
+
+
+*/
--- javascript/boilerplate-text/boilerplate-zh-hant.js
+++ javascript/boilerplate-text/boilerplate-zh-hant.js
@@ -12,8 +12,13 @@ s.rtlAttribute = ""
 s.ltrAttribute = ""
 s.rlm = ""
 
-
 // TRANSLATE THE FOLLOWING
+// but do not translate anything inside ${...} – but do move those items where needed to fit the syntax of the translation
+
+
+// used when changing the language of the page
+// do not translate the abbreviation at the start of the line, just translate the language name
+
 s.currLang = {
   'ar':'阿拉伯文',
   'bg':'保加利亞文',
@@ -31,6 +36,7 @@ s.currLang = {
   'ja':'日文',
   'ko':'韓文',
   'nl':'荷蘭文',
+  'pes':'波斯語',
   'pl':'波蘭文',
   'pt':'葡萄牙文',
   'pt-br':'葡萄牙文',
@@ -42,102 +48,165 @@ s.currLang = {
   'uk':'烏克蘭文',
   'vi':'越南語',
   'zh-hans':'簡體中文',
-  'zh-hant':'繁體中文',
+  'zh-hant':'繁體中文'
 }
 
-s.i18nActivityHome = "I18N 活動主頁"
-s.moreResourcesOfThisType = "更多此類資源"
-s.accessKeyN = 'n 鍵跳至 <a href="#internal-links" accesskey="n">内頁瀏覽</a>. <a href="#contentstart">跳至内容</a>'
-s.examplesInAnotherScript = "此文檔包含其他語言/程式的例子"
-s.worldMap = "世界地圖"
-s.searchI18nSite = "I18n 網站搜索" 
-s.translationDisclaimer = '此文檔為翻譯。如有不同或錯誤<a href="'+f.filename+'.en">最新的英語原文</a> 應為依照標準.原始版權屬于 W3C, 文件内容翻譯如下所示.'
-s.relatedLinks = "相關鏈接"
-s.articles = "文章" // used in breadcrumbs, top right of page 
-s.topicIndexText = "内容索引"
-s.techIndexText = "技術索引"
-s.gotoW3cHome = "回到 W3C 主頁"
-s.gotoI18nHome = "回到國際化活動主頁"
-s.internationalizationTitle = "國際化"
-s.worldwide = "締造真正全球通行的萬維網"
-s.i18nActivityHomePage = "國際化活動主頁."
-s.home = "主頁"
-s.contactI18nActivity = "關於國際化活動."
-s.contact = "關於"
-s.aboutI18nActivity = "About W3C Internationalization."
-s.about = "About"
-s.askI18nActivity = "Ask for help or information."
-s.ask = "Ask"
-s.groupsThatMakeUp = "國際化活動的小組"
-s.groups = "小組"
-s.topicIndexForInformation = "此網站信息内容索引"
-s.topics = "内容"
-s.taskBasedIndex = "i18n 技術項目索引"
-s.techniques = "技術"
-s.informationResources = "國際化網站信息資源"
-s.resources = "資源"
-s.newsFiltersAndFeeds = "關於W3C國際化新聞過濾和RSS feeds 的内容"
-s.news = "新聞"
-s.onThisPage = "在此頁"
-s.questionAlt = "問題"
-s.questionLink = "問題"
-s.question = "問題"
-s.backgroundAlt = "背景信息"
-s.backgroundLink = "背景"
-s.background = "背景"
-s.answer = "回答" // heading
-s.answerAlt = "回答"
-s.answerLink = "回答"
-s.byTheWayAlt = "其他有用信息"
-s.byTheWayLink = "另外"
-s.byTheWay = "另外"
-s.furtherReadingAlt = "更多閲讀資料"
-s.furtherReadingLink = "更多閲讀資料"
-s.furtherReading = "更多閲讀資料"
-s.intendedAudience = "目標讀者:"
-s.skipToAnswer = "[跳至答案]"
-s.tellUsWhatYouThink = "給我們回饋意見 (英文)."
-s.sendAComment = "遞交您的意見"
-s.subscribeToRSS = "訂閲 RSS feed."
-s.newResourcesAlt = "當每次新資源第一次發表時通知您"
-s.newResources = "新資源"
-s.homePageNewsAlt = "在主頁現實的所有新聞内容"
-s.homePageNews = "主頁新聞 "
-s.followOurNews = "Follow our news feed."
-s.sentenceDelimiter = "."
+// cookie message, appears when changing languages
+s.cookieMsg = "If you let the browser set a cookie, you will continue to see W3C Internationalization Activity pages (where available) in the language you chose. Do you want to set the cookie?" // this text is to be copied to another location
 
-s.validXHTML = "有效的 XHTML 1.0!"
-s.validCSS = "有效的 CSS!"
-s.codedInUtf8 = "UTF-8! 編碼"
+// items in top right beige column
+s.worldMap = "世界地圖"  // title text for image, top right
+s.searchI18nSite = "I18n 網站搜索" // placeholder text for search box, top right
+s.translationDisclaimer = `此文檔為翻譯。如有不同或錯誤<a href="${ f.filename }.en">最新的英語原文</a> 應為依照標準.原始版權屬于 W3C, 文件内容翻譯如下所示.`  // text appears only on translated pages
 
-// this block of strings should include any whitespace needed after the colon
-// or its equivalent, eg. "By: " in english, but "作者：" in chinese
-s.author = "作者：" // followed by name of author(s)
-s.previousAuthors = "前作者：" // followed by name of previous author(s), before substantive changes were made by the current author
-s.modifiedBy = "修改：" // person's name appears after colon
-s.translatedBy = "翻譯："
-s.acknowledgements = "Thanks also to the following people whose contribution or feedback was included: " // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.articles = "文章" // used in breadcrumbs, top right of page
+s.tests="Tests" // used at the end of breadcrumbs for test related pages
+s.home = "主頁" // start of breadcrumbs
+s.onThisPage = "在此頁"  // TOC heading
 
-s.translatedFromEnglishVer = "從 "+dt.enVersion+" 英文文件翻譯而來. 翻譯版本 "+dt.thisVersionPlain+" GMT"
 
-s.historyOfDocumentChanges = 'For the history of document changes, see the <a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">news feed</a> for substantive changes, and the <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/'+f.directory+f.filename+'.en.html">Github commit list</a> for all changes since Jan 2016.'
-s.untranslatedChanges = '<strong>注意:</strong> 在此文件翻譯以後<a href="'+f.filename+'.en">英文版本</a> 已有改變。 The changes were incorporated into this page, so you may see some passages in English until the translation is updated.'
-s.unlinkedTranslation = '<strong>Warning:</strong> Substantive changes have been made to the English original since this document was translated. You are advised to read <a href="'+f.filename+'.en">the English version</a> until this translation has been updated.'
-s.new="新"// placed alongside new articles at http://www.w3.org/International/articlelist 
-s.updated="更新：" // same as New
-s.translation_updated="更新譯本:" // date appears after colon 
 
+
+// messages that appear in the top right beige column before an article is published
 s.aboutThisArticle="關於本文" // title in the right column near the top of the page
 s.status_draft="This article is a draft that has not yet gone through public review. If you have comments, please send them using the <a href='#survey'>link near the bottom of this page</a>."
 s.status_review="This article is currently out for public review. If you have comments, please send them using the <a href='#survey'>link near the bottom of this page</a>."
 s.status_published="This article has been reviewed by the W3C Internationalization Working Group and has gone through public review to make it as accurate as possible. If you have comments, please send them using the <a href='#survey'>link near the bottom of this page</a>."
 s.status_notreviewed="This article was published without public review. If you have comments, please send them using the <a href='#survey'>link near the bottom of this page</a>."
 s.status_obsolete="This article is now obsolete. It is no longer maintained and is likely to be inaccurate. For more up-to-date information, see the <a href='http://www.w3.org/International/'>Internationalization Activity home page</a>."
-s.tests="Tests" // used at the end of breadcrumbs for test related pages
+
+
+// top left of page
+s.gotoW3cHome = "回到 W3C 主頁"  // title text for W3C logo
+s.gotoI18nHome = "回到國際化活動主頁" // title text for i18n banner
+s.internationalizationTitle = "國際化"  // the word above the orange line
+s.worldwide = "締造真正全球通行的萬維網"  // the words below the orange line
+s.techniques = "技術"  // site links link text, top left
+s.taskBasedIndex = "i18n 技術項目索引"  // title text for s.techniques
+s.resources = "資源"  // site links link text, top left
+s.informationResources = "國際化網站信息資源"  // title text for s.resources
+s.ask = "Ask"  // site links link text, top left
+s.askI18nActivity = "Ask for help or information."  // title text for s.ask
+s.news = "新聞"  // site links link text, top left
+s.newsFiltersAndFeeds = "關於W3C國際化新聞過濾和RSS feeds 的内容"  // title text for s.news
+s.groups = "小組" // site links link text, top left
+s.groupsThatMakeUp = "國際化活動的小組" // title text for s.groups
+s.about = "About" // site links link text, top left
+s.aboutI18nActivity = "About W3C Internationalization." // title text for s.about
+
+
+// document status information, below main heading
+s.intendedAudience = "目標讀者:"  // preface to audience description (being faded out)
+s.lastChanged = "Last changed "
+
+
+// bottom right comment and news feed box
+s.tellUsWhatYouThink = "給我們回饋意見 (英文)."  // text content
+s.sendAComment = "遞交您的意見" // link text
+
+
+// page footer
+// this block of strings should include any whitespace needed after the colon
+// or its equivalent, eg. "By: " in english, but "作者：" in chinese
+s.sentenceDelimiter = "."  // provides a sentence terminator to use between list of authors, modifiers, & translators
+s.author = "作者：" // followed by name of author(s)
+s.previousAuthors = "前作者：" // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "修改：" // person's name appears after colon
+s.translatedBy = "翻譯：" // person's name appears after colon
+s.acknowledgements = "Thanks also to the following people whose contribution or feedback was included: " // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.translatedFromEnglishVer = `從 ${ dt.enVersion } 英文文件翻譯而來. 翻譯版本 ${ dt.thisVersionPlain } GMT`
+s.translation_updated="更新譯本:" // date appears after (add colon+whitespace) 
+s.historyOfDocumentChanges = `For the history of document changes, see the <a href="http://www.w3.org/blog/International/tag/${ f.searchString }/">news feed</a> for substantive changes, and the <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/${ f.directory }${ f.filename }.en.html">Github commit list</a> for all changes since Jan 2016.`
+
+
+// banner on translated pages that are missing some translation
+s.untranslatedChanges = `<strong>注意:</strong> 在此文件翻譯以後<a href="${ f.filename }.en">英文版本</a> 已有改變。 The changes were incorporated into this page, so you may see some passages in English until the translation is updated.`
+s.unlinkedTranslation = `<strong>Warning:</strong> Substantive changes have been made to the English original since this document was translated. You are advised to read <a href="${ f.filename }.en">the English version</a> until this translation has been updated.`
+s.githubRedirect = `<strong>NOTE!</strong> &nbsp; This is only an editor's draft of this article. All links and bookmarks should point to the <a href='https://www.w3.org/International/${ f.directory }${ f.filename }.${ f.clang }'>version on the W3C site</a>.` // used for github-based versions of published articles
+
+
+s.new="新"// placed alongside new articles at https://www.w3.org/International/articlelist 
+s.updated="更新：" // same as New
+
+
+// strings to promote consistency in article content
+s.question = "問題"  // heading
+s.questionAlt = "問題"  // title text for s.question
+s.questionLink = "問題"  // 
+s.skipToAnswer = "[跳至答案]"  // link text
+s.background = "背景" // heading
+s.backgroundAlt = "背景信息" // title text for s.background
+s.backgroundLink = "背景"
+s.answer = "回答" // heading
+s.answerAlt = "回答" // title text for s.answer
+s.answerLink = "回答"
+s.byTheWay = "另外" // heading
+s.byTheWayAlt = "其他有用信息" // title text for s.byTheWay
+s.byTheWayLink = "另外"
+s.furtherReading = "更多閲讀資料" // heading
+s.furtherReadingAlt = "更多閲讀資料" // title text for s.byTheWay
+s.furtherReadingLink = "更多閲讀資料"
 s.quickanswer = "Quick answer" // heading
 s.longeranswer = "Details" // heading that follows 'Quick answer'
 s.additionalinfo = "Additional information" // heading that sometimes follows 'Details'
-s.cookieMsg = "If you let the browser set a cookie, you will continue to see W3C Internationalization Activity pages (where available) in the language you chose. Do you want to set the cookie?" // this text is to be copied to another location
-s.githubRedirect = "<strong>NOTE!</strong> &nbsp; This is only an editor\'s draft of this article. All links and bookmarks should point to the <a href='https://www.w3.org/International/"+f.directory+f.filename+"."+f.clang+"'>version on the W3C site</a>." // used for github-based versions of published articles
 
-s.lastChanged = "Last changed "
+
+// obsolete in most recent articles — used to be in bottom right box
+s.subscribeToRSS = "訂閲 RSS feed."  
+s.newResourcesAlt = "當每次新資源第一次發表時通知您"
+s.newResources = "新資源"
+s.homePageNewsAlt = "在主頁現實的所有新聞内容"
+s.homePageNews = "主頁新聞 "
+s.followOurNews = "Follow our news feed."
+
+// obsolete in most recent articles — used to be in footer
+s.validXHTML = "有效的 XHTML 1.0!"
+s.validCSS = "有效的 CSS!"
+s.codedInUtf8 = "UTF-8! 編碼"
+
+
+
+// other — may be obsolete, or used in old format pages
+s.i18nActivityHome = "I18N 活動主頁"
+s.moreResourcesOfThisType = "更多此類資源"
+s.accessKeyN = `n 鍵跳至 <a href="#internal-links" accesskey="n">内頁瀏覽</a>. <a href="#contentstart">跳至内容</a>`
+s.examplesInAnotherScript = "此文檔包含其他語言/程式的例子"
+s.relatedLinks = "相關鏈接"
+s.techIndexText = "技術索引"
+s.topicIndexText = "内容索引"
+s.i18nActivityHomePage = "國際化活動主頁."
+s.topicIndexForInformation = "此網站信息内容索引"
+s.topics = "内容"
+
+
+
+/*
+
+MISSING TRANSLATIONS
+
+cookieMsg
+tests
+status_draft
+status_review
+status_published
+status_notreviewed
+status_obsolete
+
+ask
+askI18nActivity
+about
+aboutI18nActivity
+lastChanged
+acknowledgements
+historyOfDocumentChanges
+untranslatedChanges
+unlinkedTranslation
+githubRedirect
+quickanswer
+longeranswer
+additionalinfo
+followOurNews
+
+
+
+*/
