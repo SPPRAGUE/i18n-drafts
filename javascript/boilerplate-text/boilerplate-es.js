88b33d090a555e67ae0f3337d88d90c272e3ec8c
r12a <ishida@w3.org>
2025-05-10 07:28:15 +0100

    boilerplate-es   Apply new order. List needed translations.

--- javascript/boilerplate-text/boilerplate-es.js
+++ javascript/boilerplate-text/boilerplate-es.js
@@ -12,8 +12,13 @@ s.rtlAttribute = ""
 s.ltrAttribute = ""
 s.rlm = ""
 
-
 // TRANSLATE THE FOLLOWING
+// but do not translate anything inside ${...} – but do move those items where needed to fit the syntax of the translation
+
+
+// used when changing the language of the page
+// do not translate the abbreviation at the start of the line, just translate the language name
+
 s.currLang = {
   'ar':'Árabe',
   'bg':'Búlgaro',
@@ -31,6 +36,7 @@ s.currLang = {
   'ja':'Japonés',
   'ko':'Coreano',
   'nl':'Neerlandés',
+  'pes':'Persa',
   'pl':'Polaco',
   'pt':'Portugués',
   'pt-br':'Portugués brasileño',
@@ -45,102 +51,148 @@ s.currLang = {
   'zh-hant':'Chino tradicional'
 }
 
-s.i18nActivityHome = "Página de inicio de internacionalización"
-s.moreResourcesOfThisType = "Más recursos relacionados con la internacionalización."
-s.accessKeyN = 'La tecla de acceso "n" lleva a la navegación de la página. <a href="#contentstart">Ir al inicio del contenido.</a>'
-s.examplesInAnotherScript = "Este documento contiene ejemplos en otros idiomas y escrituras."
-s.worldMap = "Mapa mundial"
-s.searchI18nSite = "Buscar en el sitio de internacionalización" 
-s.translationDisclaimer = 'Este documento es una traducción realizada por un voluntario. En el caso de discrepancias o errores, la <a href="'+f.filename+'.en">última versión original en inglés</a> será considerada como fidedigna. Los <a href="#copyright">derechos de autor originales</a> pertenecen a W3C, tal como se indica al final de la página.'
-s.relatedLinks = "Enlaces relacionados"
-s.articles = "Artículos" // used in breadcrumbs, top right of page 
-s.topicIndexText = "Temario"
-s.techIndexText = "Índice de técnicas"
-s.gotoW3cHome = "Ir a la página principal de W3C"
-s.gotoI18nHome = "Ir a la página principal de actividades de internacionalización"
-s.internationalizationTitle = "Internacionalización"
-s.worldwide = "Haciendo la Web global!"
-s.i18nActivityHomePage = "Página principal de actividades de internacionalización."
-s.home = "Inicio"
-s.contactI18nActivity = "Obtener ayuda del grupo de Internacionalización del W3C."
-s.contact = "Contacto"
-s.aboutI18nActivity = "About W3C Internationalization."
-s.about = "About"
-s.askI18nActivity = "Ask for help or information."
-s.ask = "Ask"
-s.groupsThatMakeUp = "Participar en el trabajo de Internacionalización del W3C."
-s.groups = "Participar"
-s.topicIndexForInformation = "Buscar en este sitio."
-s.topics = "Buscar"
-s.taskBasedIndex = "Aprender técnicas y requisitos para la internacionalización."
-s.techniques = "Aprender"
-s.informationResources = "Encontrar información en este sitio."
-s.resources = "Encontrar"
-s.newsFiltersAndFeeds = "Seguir al grupo de Internacionalización del W3C."
-s.news = "Seguir"
-s.onThisPage = "En esta página"
-s.questionAlt = "Pregunta"
-s.questionLink = "Pregunta"
-s.question = "Pregunta"
-s.backgroundAlt = "Información preliminar"
-s.backgroundLink = "Información preliminar"
-s.background = "Información preliminar"
-s.answer = "Respuesta" // heading
-s.answerAlt = "Respuesta"
-s.answerLink = "Respuesta"
-s.byTheWayAlt = "Información útil adicional"
-s.byTheWayLink = "A propósito"
-s.byTheWay = "A propósito"
-s.furtherReadingAlt = "Lecturas complementarias"
-s.furtherReadingLink = "Lecturas complementarias"
-s.furtherReading = "Lecturas complementarias"
-s.intendedAudience = "Audiencia de destino:"
-s.skipToAnswer = "[Pasar directamente a la respuesta]"
-s.tellUsWhatYouThink = "Dinos qué piensas."
-s.sendAComment = "Envíanos un comentario"
-s.subscribeToRSS = "Suscripción a feed RSS."
-s.newResourcesAlt = "Te notifica la primera vez que se publica un nuevo recurso."
-s.newResources = "Nuevos recursos"
-s.homePageNewsAlt = "Todos los elementos mostrados en la página de inicio."
-s.homePageNews = "Noticias de la página de inicio"
-s.followOurNews = "Siga nuestras noticias."
-s.sentenceDelimiter = "."
+// cookie message, appears when changing languages
+s.cookieMsg = "Si permite que el navegador establezca una cookie, continuará viendo las páginas de Actividad de Internacionalización de W3C (si existieran) en el idioma de su elección. ¿Quiere establecer la cookie?" // this text is to be copied to another location
 
-s.validXHTML = "XHTML 1.0 válido"
-s.validCSS = "CSS válido"
-s.codedInUtf8 = "Codificado en UTF-8"
+// items in top right beige column
+s.worldMap = "Mapa mundial"  // title text for image, top right
+s.searchI18nSite = "Buscar en el sitio de internacionalización" // placeholder text for search box, top right
+s.translationDisclaimer = `Este documento es una traducción realizada por un voluntario. En el caso de discrepancias o errores, la <a href="${ f.filename }.en">última versión original en inglés</a> será considerada como fidedigna. Los <a href="#copyright">derechos de autor originales</a> pertenecen a W3C, tal como se indica al final de la página.`  // text appears only on translated pages
 
-// this block of strings should include any whitespace needed after the colon
-// or its equivalent, eg. "By: " in english, but "作者：" in chinese
-s.author = "De: " // followed by name of author(s)
-s.previousAuthors = "Anteriormente de: " // followed by name of previous author(s), before substantive changes were made by the current author
-s.modifiedBy = "Cambiado por: " // person's name appears after colon
-s.translatedBy = "Traducido por: "
-s.acknowledgements = "Agradecemos también a las siguientes personas cuyas contribuciones y opiniones fueron incluidas: " // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.articles = "Artículos" // used in breadcrumbs, top right of page
+s.tests="Tests" // used at the end of breadcrumbs for test related pages
+s.home = "Inicio" // start of breadcrumbs
+s.onThisPage = "En esta página"  // TOC heading
 
-s.translatedFromEnglishVer = "Traducido del inglés con fecha "+dt.enVersion+". Traducción modificada por última vez el "+dt.thisVersionPlain+" GMT."
 
-s.historyOfDocumentChanges = 'Para el historial de cambios del documento, consultar la <a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">fuente de noticias</a> para ver los cambios sustanciales, y la <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/'+f.directory+f.filename+'.en.html">lista de commits de Github</a> para ver todos los cambios desde enero 2016.'
-s.untranslatedChanges = '<strong>Nota:</strong> Desde la traducción de este documento se han producido modificaciones en el <a href="'+f.filename+'.en.php">original en inglés</a>. Los cambios se incorporaron en esta página, por lo cual es posible que vea algunas partes en inglés hasta que se actualice la traducción.'
-s.unlinkedTranslation = '<strong>Advertencia:</strong> Desde que se tradujo este documento, se introdujeron cambios sustanciales en la versión original en inglés. Le sugerimos leer <a href="'+f.filename+'.en">una versión en otro idioma</a> hasta que esta traducción haya sido actualizada.'
-s.new="¡Nuevo!"// placed alongside new articles at http://www.w3.org/International/articlelist 
-s.updated="¡Actualizado!" // same as New
-s.translation_updated="Traducción actualizada el:" // date appears after colon 
 
+
+// messages that appear in the top right beige column before an article is published
 s.aboutThisArticle="Acerca de este artículo" // title in the right column near the top of the page
-s.status_draft="This article is a draft that has not yet gone through public review. If you have comments, please send them using the <a href='#survey'>link near the bottom of this page</a>."
 s.status_draft="Este artículo es un borrador que todavía no se ha sometido a revisión pública. Si hay asuntos que es necesario tratar, envíenos sus comentarios mediante <a href='#survey'>el enlace ubicado cerca del final de la página</a>."
 s.status_review="En este momento, este artículo está expuesto para revisión pública. Si hay asuntos que es necesario tratar, envíenos sus comentarios mediante <a href='#survey'>el enlace ubicado cerca del final de la página</a>."
 s.status_published="El Grupo de Trabajo de Internacionalización de W3 ha revisado este artículo y se lo ha sometido a revisión pública para hacerlo tan preciso como sea posible. Si hay asuntos que es necesario tratar, envíenos sus comentarios mediante <a href='#survey'>el enlace ubicado cerca del final de la página</a>."
 s.status_notreviewed="Este artículo se publicó sin revisión pública. Si hay asuntos que es necesario tratar, envíenos sus comentarios mediante <a href='#survey'>el enlace ubicado cerca del final de la página</a>."
 s.status_obsolete="Este artículo ahora es obsoleto. Ya no recibe mantenimiento y es probable que no contenga información precisa. Para información más actualizada, vea la <a href='http://www.w3.org/International/'>página de Actividad de Internacionalización</a>."
-s.tests="" // used at the end of breadcrumbs for test related pages
-s.quickanswer = "Respuesta rápida" // heading
-s.longeranswer = "Respuesta extensa" // heading
-s.details = "Detalles" // heading
-s.additionalinfo = "Información adicional" // heading that sometimes follows 'Details'
-s.cookieMsg = "Si permite que el navegador establezca una cookie, continuará viendo las páginas de Actividad de Internacionalización de W3C (si existieran) en el idioma de su elección. ¿Quiere establecer la cookie?" // this text is to be copied to another location
-//s.supercededTranslation = '<strong>Advertencia:</strong> ¡Esta es una versión desactualizada de este documento! Le recomendamos leer  <a href="'+g.betterfilename+'.en">la última versión</a> et si vous le pouvez, y, si puede, cambiar la marca o el enlace que lo trajo aquí.'
-s.githubRedirect = "<strong>NOTA!</strong> &nbsp; Este es sólo un borrador del artículo del editor. Todos los enlaces y marcadores deben apuntar a  la <a href='https://www.w3.org/International/"+f.directory+f.filename+"."+f.clang+"'>versión del sitio web de W3C</a>." // used for github-based versions of published articles
 
+
+// top left of page
+s.gotoW3cHome = "Ir a la página principal de W3C"  // title text for W3C logo
+s.gotoI18nHome = "Ir a la página principal de actividades de internacionalización" // title text for i18n banner
+s.internationalizationTitle = "Internacionalización"  // the word above the orange line
+s.worldwide = "Haciendo la Web global!"  // the words below the orange line
+s.techniques = "Aprender"  // site links link text, top left
+s.taskBasedIndex = "Aprender técnicas y requisitos para la internacionalización."  // title text for s.techniques
+s.resources = "Encontrar"  // site links link text, top left
+s.informationResources = "Encontrar información en este sitio."  // title text for s.resources
+s.ask = "Ask"  // site links link text, top left
+s.askI18nActivity = "Ask for help or information."  // title text for s.ask
+s.news = "Seguir"  // site links link text, top left
+s.newsFiltersAndFeeds = "Seguir al grupo de Internacionalización del W3C."  // title text for s.news
+s.groups = "Participar" // site links link text, top left
+s.groupsThatMakeUp = "Participar en el trabajo de Internacionalización del W3C." // title text for s.groups
+s.about = "About" // site links link text, top left
+s.aboutI18nActivity = "About W3C Internationalization." // title text for s.about
+
+
+// document status information, below main heading
+s.intendedAudience = "Audiencia de destino:"  // preface to audience description (being faded out)
 s.lastChanged = "Última actualización "
+
+
+// bottom right comment and news feed box
+s.tellUsWhatYouThink = "Dinos qué piensas."  // text content
+s.sendAComment = "Envíanos un comentario" // link text
+
+
+// page footer
+// this block of strings should include any whitespace needed after the colon
+// or its equivalent, eg. "By: " in english, but "作者：" in chinese
+s.sentenceDelimiter = "."  // provides a sentence terminator to use between list of authors, modifiers, & translators
+s.author = "De: " // followed by name of author(s)
+s.previousAuthors = "Anteriormente de: " // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "Cambiado por: " // person's name appears after colon
+s.translatedBy = "Traducido por: " // person's name appears after colon
+s.acknowledgements = "Agradecemos también a las siguientes personas cuyas contribuciones y opiniones fueron incluidas: " // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.translatedFromEnglishVer = `Traducido del inglés con fecha ${ dt.enVersion }. Traducción modificada por última vez el ${ dt.thisVersionPlain } GMT.`
+s.translation_updated="Traducción actualizada el:" // date appears after (add colon+whitespace) 
+s.historyOfDocumentChanges = `Para el historial de cambios del documento, consultar la <a href="http://www.w3.org/blog/International/tag/${ f.searchString }/">fuente de noticias</a> para ver los cambios sustanciales, y la <a href="https://github.com/w3c/i18n-drafts/commits/gh-pages/${ f.directory }${ f.filename }.en.html">lista de commits de Github</a> para ver todos los cambios desde enero 2016.`
+
+
+// banner on translated pages that are missing some translation
+s.untranslatedChanges = `<strong>Nota:</strong> Desde la traducción de este documento se han producido modificaciones en el <a href="${ f.filename }.en.php">original en inglés</a>. Los cambios se incorporaron en esta página, por lo cual es posible que vea algunas partes en inglés hasta que se actualice la traducción.`
+s.unlinkedTranslation = `<strong>Advertencia:</strong> Desde que se tradujo este documento, se introdujeron cambios sustanciales en la versión original en inglés. Le sugerimos leer <a href="${ f.filename }.en">una versión en otro idioma</a> hasta que esta traducción haya sido actualizada.`
+s.githubRedirect = `<strong>NOTA!</strong> &nbsp; Este es sólo un borrador del artículo del editor. Todos los enlaces y marcadores deben apuntar a  la <a href='https://www.w3.org/International/${ f.directory }${ f.filename }.${ f.clang }'>versión del sitio web de W3C</a>.` // used for github-based versions of published articles
+
+
+s.new="¡Nuevo!"// placed alongside new articles at https://www.w3.org/International/articlelist 
+s.updated="¡Actualizado!" // same as New
+
+
+// strings to promote consistency in article content
+s.question = "Pregunta"  // heading
+s.questionAlt = "Pregunta"  // title text for s.question
+s.questionLink = "Pregunta"  // 
+s.skipToAnswer = "[Pasar directamente a la respuesta]"  // link text
+s.background = "Información preliminar" // heading
+s.backgroundAlt = "Información preliminar" // title text for s.background
+s.backgroundLink = "Información preliminar"
+s.answer = "Respuesta" // heading
+s.answerAlt = "Respuesta" // title text for s.answer
+s.answerLink = "Respuesta"
+s.byTheWay = "A propósito" // heading
+s.byTheWayAlt = "Información útil adicional" // title text for s.byTheWay
+s.byTheWayLink = "A propósito"
+s.furtherReading = "Lecturas complementarias" // heading
+s.furtherReadingAlt = "Lecturas complementarias" // title text for s.byTheWay
+s.furtherReadingLink = "Lecturas complementarias"
+s.quickanswer = "Respuesta rápida" // heading
+s.longeranswer = "Respuesta extensa" // heading that follows 'Quick answer'
+s.additionalinfo = "Información adicional" // heading that sometimes follows 'Details'
+
+
+// obsolete in most recent articles — used to be in bottom right box
+s.subscribeToRSS = "Suscripción a feed RSS."  
+s.newResourcesAlt = "Te notifica la primera vez que se publica un nuevo recurso."
+s.newResources = "Nuevos recursos"
+s.homePageNewsAlt = "Todos los elementos mostrados en la página de inicio."
+s.homePageNews = "Noticias de la página de inicio"
+s.followOurNews = "Siga nuestras noticias."
+
+// obsolete in most recent articles — used to be in footer
+s.validXHTML = "XHTML 1.0 válido"
+s.validCSS = "CSS válido"
+s.codedInUtf8 = "Codificado en UTF-8"
+
+
+
+// other — may be obsolete, or used in old format pages
+s.i18nActivityHome = "Página de inicio de internacionalización"
+s.moreResourcesOfThisType = "Más recursos relacionados con la internacionalización."
+s.accessKeyN = `La tecla de acceso "n" lleva a la navegación de la página. <a href="#contentstart">Ir al inicio del contenido.</a>`
+s.examplesInAnotherScript = "Este documento contiene ejemplos en otros idiomas y escrituras."
+s.relatedLinks = "Enlaces relacionados"
+s.techIndexText = "Índice de técnicas"
+s.topicIndexText = "Temario"
+s.i18nActivityHomePage = "Página principal de actividades de internacionalización."
+s.topicIndexForInformation = "Buscar en este sitio."
+s.topics = "Buscar"
+
+
+
+/*
+
+MISSING TRANSLATIONS
+
+check all top left links !
+
+s.tests
+s.ask
+s.askI18nActivity
+s.about
+s.aboutI18nActivity
+
+
+
+
+*/
